d0163dace7ce8b60297f369748977a627dba50b6
wenqi711 <152916174+wenqi711@users.noreply.github.com>
2025-12-14 19:28:53 +0400

    Write code for getComputerChoice()

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
-console.log("Hello World!")
+function getComputerChoice() {
+
+    // SET randomNumber to Math.random
+    randomNumber = Math.random();
+
+    // IF randomNumber < 1/3 THEN
+    if (randomNumber < 1/3) {
+        //   DISPLAY computerChoice as "Rock"
+        return "Rock"
+    }
+    // ELSE IF randomNumber > 2/3 THEN
+    else if (randomNumber > 2/3) {
+        //   DISPLAY computerChoice as "Paper"
+        return "Paper"
+    }
+    // ELSE
+    else {
+        //   DISPLAY computerChoice as "Scissors"
+        return "Scissors"
+    }
+    // ENDIF
+
+    return "No choice possible"
+}
