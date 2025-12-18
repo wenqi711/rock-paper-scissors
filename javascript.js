9860c4ae59abbebb26185143130f759a6c5d8e61
wenqi711 <152916174+wenqi711@users.noreply.github.com>
2025-12-18 10:00:46 +0400

    Add start/end game, play again buttons

--- javascript.js
+++ javascript.js
@@ -16,33 +16,30 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
 
     if (computerChoice === humanChoice) {
-        resultsContainer.textContent = `It's a tie!
-        Player score: ${humanScore}, Computer score: ${computerScore}`;
+        resultsContainer.textContent = `It's a tie!`;
     } 
     else if ((humanChoice === "rock" && computerChoice === "scissors")
     || (humanChoice === "scissors" && computerChoice === "paper")
     || (humanChoice === "paper" && computerChoice === "rock")) {
         ++humanScore;
-        resultsContainer.textContent = `You win! ${humanChoice} beats ${computerChoice}!
-        Player score: ${humanScore}, Computer score: ${computerScore}`;
+        resultsContainer.textContent = `You win! ${humanChoice} beats ${computerChoice}!`;
 
     } 
     else if ((humanChoice === "rock" && computerChoice === "paper")
     || (humanChoice === "scissors" && computerChoice === "rock")
     || (humanChoice === "paper" && computerChoice === "scissors")) {
         ++computerScore;
-        resultsContainer.textContent = `You lose! ${computerChoice} beats ${humanChoice}!
-        Player score: ${humanScore}, Computer score: ${computerScore}`;
+        resultsContainer.textContent = `You lose! ${computerChoice} beats ${humanChoice}!`;
     }
+
+    scoreContainer.textContent = `Player score: ${humanScore}, Computer score: ${computerScore}`;
     return;
 }
 
-function playAgain() {
-    return;
-}
-
+// Declare score variables
 let humanScore = 0;
 let computerScore = 0;
+let gameResult = "";
 
 // Create rock button
 const rockButton = document.createElement("button");
@@ -59,19 +56,40 @@ const scissorsButton = document.createElement("button");
 scissorsButton.textContent = "Scissors";
 scissorsButton.classList.add("scissors-button");
 
+// Create "Play Again", "Start Game", "End Game" buttons
+const playAgainButton = document.createElement("button");
+playAgainButton.textContent = "Play Again?";
+playAgainButton.classList.add("play-again-button");
+
+const startGameButton = document.createElement("button");
+startGameButton.textContent = "Start Game";
+startGameButton.classList.add("start-game-button");
+
+const endGameButton = document.createElement("button");
+endGameButton.textContent = "End Game";
+endGameButton.classList.add("end-game-button");
+
+// Create containers for holding buttons, results, scores
 const mainBody = document.querySelector("body");
 const buttonContainer = document.createElement("div");
 mainBody.appendChild(buttonContainer);
-buttonContainer.appendChild(rockButton);
-buttonContainer.appendChild(paperButton);
-buttonContainer.appendChild(scissorsButton);
+buttonContainer.appendChild(startGameButton);
 
 const resultsContainer = document.createElement("div");
 mainBody.appendChild(resultsContainer);
 
+const scoreContainer = document.createElement("div");
+mainBody.appendChild(scoreContainer);
+
+// Button functionality
 buttonContainer.addEventListener("click", (event) => {
     let target = event.target;
     switch (target.classList.value) {
+        case 'start-game-button':
+            buttonContainer.removeChild(startGameButton);
+            addGameButtons();
+            scoreContainer.textContent = `Player score: ${humanScore}, Computer score: ${computerScore}`;
+            break;
         case 'rock-button':
             playRound("rock", getComputerChoice());
             isGameEnd();
@@ -84,21 +102,58 @@ buttonContainer.addEventListener("click", (event) => {
             playRound("scissors", getComputerChoice());
             isGameEnd();
             break;
+        case 'play-again-button':
+            resultsContainer.textContent = "";
+            scoreContainer.textContent = `Player score: ${humanScore}, Computer score: ${computerScore}`;
+            removeEndGameButtons();
+            addGameButtons();
+            break;
+        case 'end-game-button':
+            resultsContainer.textContent = "Thank you for playing!";
+            scoreContainer.textContent = "";
+            removeEndGameButtons();
     }
 });
 
 function isGameEnd() {
     if (humanScore === 5 || computerScore === 5) {
-        buttonContainer.removeChild(rockButton);
-        buttonContainer.removeChild(paperButton);
-        buttonContainer.removeChild(scissorsButton);
         if (humanScore > computerScore) {
             resultsContainer.textContent = "Congratulations! You win!";
         } else if (humanScore < computerScore) {
             resultsContainer.textContent = "You lose! Better luck next time!";
         } else {
             resultsContainer.textContent = "It's a tie!";
-        }
+        };
+        humanScore = 0;
+        computerScore = 0;
+        removeGameButtons();
+        addEndGameButtons();
     }
+    return;
+}
+
+function addGameButtons() {
+    buttonContainer.appendChild(rockButton);
+    buttonContainer.appendChild(paperButton);
+    buttonContainer.appendChild(scissorsButton);
+    return;
+}
+
+function removeGameButtons() {
+    buttonContainer.removeChild(rockButton);
+    buttonContainer.removeChild(paperButton);
+    buttonContainer.removeChild(scissorsButton);
+    return;
+}
+
+function addEndGameButtons() {
+    buttonContainer.appendChild(playAgainButton);
+    buttonContainer.appendChild(endGameButton);
+    return;
+}
+
+function removeEndGameButtons() {
+    buttonContainer.removeChild(playAgainButton);
+    buttonContainer.removeChild(endGameButton);
     return;
 }
